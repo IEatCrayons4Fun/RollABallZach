d59b53dd468e3714bb9a909ff06a376f9fd591ef
CodingIsnotFun <zacharyj0101@gmail.com>
2025-10-30 11:36:50 -0400

    Day 1

---
+++ Assets/BallScripts/PlayerController1.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerController1 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    void OnMove(InputValue movementValue)
+    {
+        
+
+    }
+        
+    }
+
