fd3ed7a3c2eae0559cd8b3b864d3711116d75322
CodingIsFun <zacharyj0101@gmail.com>
2025-11-13 11:39:33 -0500

    11/13/2025 - Roll a Ball
    Made a count text for picking up pickups , added a win text for when you collected all the pickups.

--- Assets/Scipts/PlayerController.cs
+++ Assets/Scipts/PlayerController.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
+    public GameObject winTextObject;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        SetCountText();
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        winTextObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -25,11 +32,26 @@ public class PlayerController : MonoBehaviour
         movementX = movementVector.x;
         movementY = movementVector.y; 
     }
+    void SetCountText(){
+        countText.text = "Count: " + count.ToString();
+        if (count == 18){
+            winTextObject.SetActive(true);
+        }
+    }
         
     private void FixedUpdate(){
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
 
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        } 
     }
 }
 
