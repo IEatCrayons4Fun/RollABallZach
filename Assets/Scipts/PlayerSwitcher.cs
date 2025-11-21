34299054185809fd468ef5c80271749a3c5cb8c9
star-fabi <zacharyj0101@gmail.com>
2025-11-20 22:25:18 -0500

    Added tons of Stuff

---
+++ Assets/Scipts/PlayerSwitcher.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerSwitcher : MonoBehaviour
+{
+    public GameObject capsulePrefab; // assign in Inspector
+    private bool switched = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!switched && other.CompareTag("Player"))
+        {
+            // Save position of ball before destroying
+            Vector3 pos = other.transform.position;
+
+            // Use upright rotation for capsule
+            Quaternion uprightRotation = Quaternion.Euler(0, other.transform.eulerAngles.y, 0);
+
+            // Destroy ball player (and its camera)
+            Destroy(other.gameObject);
+
+            // Spawn capsule player at same spot
+            Instantiate(capsulePrefab, pos, uprightRotation);
+
+            switched = true;
+        }
+    }
+}
